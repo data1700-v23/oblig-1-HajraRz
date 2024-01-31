51f43ff4ccefe3d74ef52a19d7158d59d57e80b0
HajraRz <s375802@oslomet.no>
2024-01-31 15:13:22 +0100

    Opprettelse av Spring Boot-prosjektet og oppdateirng av README-filen.

---
+++ src/test/java/com/example/oblig1_webprog/Oblig1WebprogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1_webprog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1WebprogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
